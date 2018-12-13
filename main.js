b7cdb55a58a3b88668b49c5e2a8320318b1bba27
Abhinav Srivastava <forwork.zero@gmail.com>
2018-12-13 15:46:59 +0530

    added apis for admin portal

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ function getData(value) {
     console.log(value);
     if(value==0){
         //url for technical
-        // durl = 
+        durl = "https://mstc-ccr-admin.herokuapp.com/tech";
     }
     else if(value==1){
         //url for management
-        //durl =
+        durl = "https://mstc-ccr-admin.herokuapp.com/management";
     }
     else{
         //url for design
-        //durl =
+        durl = "https://mstc-ccr-admin.herokuapp.com/design"
     }
 
     db();
@@ -30,7 +30,7 @@ function db(){
             $("#data").fadeIn(1000);
             let count=0; //for serial number
             for(let i in global){
-            $("#rows").append("<tr><td>"+(count++)+"</td><td class='text-primary' style='text-decoration:underline;cursor: pointer' onclick='disp(this.innerHTML)'>"+global[i].regno+"</td><td>"+global[i].name+"</td><td>"+global[i].eval+"</td></tr>")
+            $("#rows").append("<tr><td>"+(++count)+"</td><td class='text-primary' style='text-decoration:underline;cursor: pointer' onclick='disp(this.innerHTML)'>"+global[i].reg+"</td><td>"+global[i].name+"</td><td>"+global[i].evaluated+"</td></tr>")
             }
         },
         error: function(){
@@ -41,37 +41,38 @@ function db(){
 let regno;
 //function for displaying responses
 function disp(param) {
+    regno=param;
     $("#data").fadeOut(1000,function() {
         $("#respo").fadeIn(1000);
     });
     for(let i in global){
-        if(global[i].regno==param){
-            $("#q1").html(global[i].question1);
-            $("#a1").html(global[i].answer1);
+        if(global[i].reg==param){
+            $("#q1").text(global[i].question1);
+            $("#a1").text(global[i].answer1);
 
-            $("#q2").html(global[i].question2);
-            $("#a2").html(global[i].answer2);
+            $("#q2").text(global[i].question2);
+            $("#a2").text(global[i].answer2);
 
-            $("#q3").html(global[i].question3);
-            $("#a3").html(global[i].answer3);
+            $("#q3").text(global[i].question3);
+            $("#a3").text(global[i].answer3);
 
-            $("#q4").html(global[i].question4);
-            $("#a4").html(global[i].answer4);
+            $("#q4").text(global[i].question4);
+            $("#a4").text(global[i].answer4);
 
-            $("#q5").html(global[i].question5);
-            $("#a5").html(global[i].answer5);
+            $("#q5").text(global[i].question5);
+            $("#a5").text(global[i].answer5);
 
-            $("#q6").html(global[i].question6);
-            $("#a6").html(global[i].answer6);
+            $("#q6").text(global[i].question6);
+            $("#a6").text(global[i].answer6);
 
-            $("#q7").html(global[i].question7);
-            $("#a7").html(global[i].answer7);
+            $("#q7").text(global[i].question7);
+            $("#a7").text(global[i].answer7);
 
-            $("#q8").html(global[i].question8);
-            $("#a8").html(global[i].answer8);
+            $("#q8").text(global[i].question8);
+            $("#a8").text(global[i].answer8);
 
-            $("#q9").html(global[i].question9);
-            $("#a9").html(global[i].answer9);
+            $("#q9").text(global[i].question9);
+            $("#a9").text(global[i].answer9);
 
             $("#q10").html(global[i].question10);
             $("#a10").html(global[i].answer10);
@@ -86,12 +87,15 @@ function sub() {
     var x=$("#select").is(":checked");
     console.log(x);
     let name=$("#name").val();
+    console.log(name);
+    console.log(regno);
     let body={
         "reg": regno,
         "selected": x,
         "evaluated": true,
         "evaluator": name
     };
+    console.log(body);
     $.ajax({
         type: "POST",
         data: body,
